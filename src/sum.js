c271d1a94262eef4262d47d9cb8d86b817d663fa
lilian <lilian.ceccon@le-campus-numerique.fr>
2022-01-27 14:26:10 +0100

    test

--- src/sum.js
+++ src/sum.js
@@ -1,3 +1,9 @@
 // Implement the sum function here
 export function sum(a, b) {
+    if (typeof a === "number" && typeof b === "number"){
+        let sum = a+b;
+        return sum
+    }else{
+        return "il faut que a et b soit un nombre"
+    }
 }
--- test/sum.test.js
+++ test/sum.test.js
@@ -4,3 +4,7 @@ import {sum} from "../src/sum";
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('test de chaine de caractere', ()=>{
+  expect(sum("ugjh", "hjk")).toBe("il faut que a et b soit un nombre")
+});
